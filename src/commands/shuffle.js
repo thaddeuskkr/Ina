e60116fa07c91ff6d6a1f7992a6af98d020193a6
thaddeuskkr <thaddeuskkr@gmail.com>
2022-07-19 17:11:11 +0800

    feat(shuffle): shuffle command

---
+++ src/commands/shuffle.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shuffle')
+        .setDescription('Shuffles the queue.'),
+    permissions: [],
+    checks: ['IN_VC', 'SAME_VC', 'PLAYING'],
+    async run (client, interaction, player) {
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: 'Shuffled' })
+            .setDescription(`Shuffled **${player.queue.length}** tracks.`)
+            .setColor(client.config.color)
+            .setFooter(client.config.footer);
+        await player.queue.shuffle();
+        interaction.reply({ embeds: [embed] }).then(msg => player.cleanup.push(msg));
+    }
+};
