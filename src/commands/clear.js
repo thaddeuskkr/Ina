81ff3ad9541a4c7859bf84dcf385b80216afc883
thaddeuskkr <thaddeuskkr@gmail.com>
2022-07-19 23:01:19 +0800

    feat(clear): clear queue command

---
+++ src/commands/clear.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Clears the queue of all tracks.'),
+    permissions: [],
+    checks: ['IN_VC', 'SAME_VC', 'PLAYING', 'QUEUE'],
+    async run (client, interaction, player) {
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: 'Queue cleared' })
+            .setDescription(`Removed **${player.queue.length}** tracks from the queue.`)
+            .setColor(client.config.color)
+            .setFooter(client.config.footer);
+        await player.queue.clear();
+        interaction.reply({ embeds: [embed] }).then(msg => player.cleanup.push(msg));
+    }
+};
