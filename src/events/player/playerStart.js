7aeefc50ded17b400fb5fc708d659e22f64bc63f
thaddeuskkr <thaddeuskkr@gmail.com>
2022-07-19 09:29:07 +0800

    style(playerStart): remove length from next track

--- src/events/player/playerStart.js
+++ src/events/player/playerStart.js
@@ -2,8 +2,9 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = async (client, player, track) => {
     const embed = new EmbedBuilder()
         .addFields([
-            { name: 'Now playing', value: `${track.title} - ${track.author} \`${client.util.formatTime(track.length, track.isStream)}\` (${track.requester.toString()})`, inline: false },
-            { name: 'Next in queue', value: player.queue.length > 0 ? `${player.queue[0].title} - ${player.queue[0].author} \`${client.util.formatTime(player.queue[0].length, player.queue[0].isStream)}\` (${player.queue[0].requester.toString()})` : '*No tracks in queue*', inline: false }
+            { name: 'Now playing', value: `[${track.title} - ${track.author}](${track.uri}) \`${client.util.formatTime(track.length, track.isStream)}\` (${track.requester.toString()})`, inline: false },
+            { name: 'Next in queue', value: player.queue.length > 0 ? `${player.queue[0].title} - ${player.queue[0].author} (${player.queue[0].requester.toString()})` : '*No tracks in queue*', inline: false } // Without duration for next in queue
+            // { name: 'Next in queue', value: player.queue.length > 0 ? `${player.queue[0].title} - ${player.queue[0].author} \`${client.util.formatTime(player.queue[0].length, player.queue[0].isStream)}\` (${player.queue[0].requester.toString()})` : '*No tracks in queue*', inline: false }
         ])
         .setFooter(client.config.footer)
         .setThumbnail(track.thumbnail)
