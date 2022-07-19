b7afd70e228f6eddff7756eac26bb6564080eb09
thaddeuskkr <thaddeuskkr@gmail.com>
2022-07-19 09:29:58 +0800

    feat(skip): add skip command

---
+++ src/commands/skip.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('skip')
+        .setDescription('Skips the currently playing track.'),
+    permissions: [],
+    checks: ['IN_VC', 'SAME_VC', 'PLAYING'],
+    async run (client, interaction, player) {
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: 'Skipped' })
+            .setDescription(`Skipped **${player.queue.current.title}** by **${player.queue.current.author}**.`)
+            .setColor(client.config.color)
+            .setFooter(client.config.footer);
+        await player.skip();
+        interaction.reply({ embeds: [embed] });
+    }
+};
