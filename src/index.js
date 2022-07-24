9bf07cc35dc0c3d598ce2720f32d9d2f97477b20
thaddeuskkr <thaddeuskkr@gmail.com>
2022-07-24 09:42:45 +0800

    fix(sec): revert previous changes

--- src/index.js
+++ src/index.js
@@ -34,9 +34,6 @@ const client = new Discord.Client({
         Partials.Channel
     ] 
 });
-config.lavalink_nodes.forEach(node => {
-    node.secure = Boolean(node.secure);
-});
 const kazagumo = new Kazagumo({
     defaultSearchEngine: config.defaultSearchEngine,
     send: (guildId, payload) => {
